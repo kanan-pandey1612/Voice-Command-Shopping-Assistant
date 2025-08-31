65b0dcd078ab9459fc3bf8d8598187596974b299
kanan-pandey1612 <kananpandey1612@gmail.com>
2025-08-31 14:16:01 +0530

    Configure host for Render deployment

--- api/main.py
+++ api/main.py
@@ -54,4 +54,4 @@ def suggestions_endpoint():
 # This block runs the server when you execute the file
 if __name__ == '__main__':
     # Runs the app on http://127.0.0.1:5000
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
